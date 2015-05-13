e072ba6bdd23a198e51eb293b263d18ab1357190
Daulet Baimbetov <daucloudlab@gmail.com>
2015-05-13 17:46:49 +0600

    Fragment Lifecycle demo project

---
+++ app/src/main/java/kz/abcsoft/android/demo/MainActivity.java
@@ -0,0 +1,46 @@
+package kz.abcsoft.android.demo;
+
+import android.app.Activity;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class MainActivity extends Activity {
+
+    final String LOG_TAG = "myLogs";
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d(LOG_TAG, "MainActivity onCreate");
+    }
+
+    protected void onStart() {
+        super.onStart();
+        Log.d(LOG_TAG, "MainActivity onStart");
+    }
+
+    protected void onResume() {
+        super.onResume();
+        Log.d(LOG_TAG, "MainActivity onResume");
+    }
+
+    protected void onPause() {
+        super.onPause();
+        Log.d(LOG_TAG, "MainActivity onPause");
+    }
+
+    protected void onStop() {
+        super.onStop();
+        Log.d(LOG_TAG, "MainActivity onStop");
+    }
+
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(LOG_TAG, "MainActivity onDestroy");
+    }
+
+}
